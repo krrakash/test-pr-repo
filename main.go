329ef944a4ff0afad402b0889774b31615c5546c
AkashKumarTech <akashkmr.ce.97@gmail.com>
2026-04-15 21:58:53 +0530

    model changed

--- main.go
+++ main.go
@@ -77,7 +77,7 @@ func analyzeWithGemini(patch string) (string, error) {
 	apiKey := os.Getenv("GEMINI_API_KEY")
 
 	url := fmt.Sprintf(
-		"https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash-001:generateContent?key=%s",
+		"https://generativelanguage.googleapis.com/v1/models/gemini-2.0-flash:generateContent?key=%s",
 		apiKey,
 	)
 
